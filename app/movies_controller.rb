cf08fbc852164fe81730958e8d97337c81661859
danny galimi <dannygalimi13@aim.com>
2021-02-25 21:40:11 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -62,7 +62,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  binding.pry
+  
   Movie.find_by(title:"Title",release_date: 2000, director: "Me")
   
 end
