d297606c81db9c345b1afed9ee09fc355f8b6c69
danny galimi <dannygalimi13@aim.com>
2021-02-25 21:35:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -62,9 +62,9 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  binding.pry
-  Moive.find_by(title:"Title",release_date: 2000, director: "Me")
   
+  Moive.find_by(title:"Title",release_date: 2000, director: "Me")
+  binding.pry
 end
 
 def can_find_using_where_clause_and_be_sorted
