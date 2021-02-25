1304b6bc3d8d0ae3007edab9bf4d8aacb21c8fe1
danny galimi <dannygalimi13@aim.com>
2021-02-25 21:03:25 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -52,7 +52,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.sort_by {|id| id.number}.first
+  Movie.sort_by {|movie| movie.id}.first
 end
 
 def can_find_by_multiple_attributes
