a5068f5ea612a25e9af09cd2e8ca76cd5eee7838
danny galimi <dannygalimi13@aim.com>
2021-02-25 20:44:31 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -48,8 +48,7 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.size
-end
+  Movie.length
 
 def can_find_the_first_item_from_the_database_using_id
   __
