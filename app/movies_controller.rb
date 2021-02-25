1edae66423269965c7c21e24513960f53bd88589
danny galimi <dannygalimi13@aim.com>
2021-02-25 21:32:56 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -60,6 +60,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
+  binding.pry
   Moive.find_by(title:"Title",release_date: 2000, director: "Me")
 end
 
